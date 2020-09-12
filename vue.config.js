07321a3ab7e7088b2023d0089be06fac4354412a
vietahn <bv.anh194@gmail.com>
2020-09-12 08:18:33 +0700

    d

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ module.exports = {
   devServer: {
     port: 2080,
     disableHostCheck: true,
+    // clientLogLevel: 'warning',
+    inline: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    },
   },
 };
